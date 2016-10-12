136916ddfd732259d96be5c8e2dcbfcf434e9615
Lam <tunglam.dang@gmail.com>
2016-10-12 20:50:02 +0200

    build_and_push

--- build_and_push_all.sh
+++ build_and_push_all.sh
@@ -4,6 +4,10 @@ for dir in $(ls -d */)
 do
 	echo $dir
 	docker build -t ${dir: :-1} ${dir}
+<<<<<<< HEAD
 	docker tag ${dir: :-1} ${registry}/${dir: :-1}:${tag}
+=======
+	docker tag ${registry}/${dir: :-1}:${tag} ${dir: :-1}
+>>>>>>> build_and_push
 	docker push ${registry}/${dir: :-1}:${tag}
 done
