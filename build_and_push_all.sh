aceed255b0e762dc71ed857ba35aadeac269d4a1
lam <tunglam.dang@gmail.com>
2017-01-09 22:56:14 +0100

    fix build & push

--- build_and_push_all.sh
+++ build_and_push_all.sh
@@ -3,8 +3,7 @@ tag=$2
 for dir in $(ls -d */)
 do
 	echo $dir
-	docker build -t ${registry}/${dir: :-1} ${dir}
-	docker tag ${registry}/${dir: :-1} ${registry}/${dir: :-1}:${tag}
-	docker tag ${registry}/${dir: :-1}:${tag} ${dir: :-1}
+	docker build -t ${dir: :-1} ${dir}
+	docker tag ${dir: :-1} ${registry}/${dir: :-1}:${tag}
 	docker push ${registry}/${dir: :-1}:${tag}
 done
