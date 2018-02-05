272af65968fd668c03f45e93221538cbcdcef655
lam <tunglam.dang@gmail.com>
2018-02-05 23:08:38 +0100

    finish update

--- 30_deeplearning/test.sh
+++ 30_deeplearning/test.sh
@@ -9,10 +9,8 @@ docker build -t ${image_name} $DIR
 ## Run tests
 echo "Test cuda"
 nvidia-docker run --rm ${image_name} nvidia-smi
-echo "Test theano"
-nvidia-docker run --rm ${image_name} python -c "import theano"
-echo "Test keras w theano"
-nvidia-docker run --rm ${image_name} bash -c "KERAS_BACKEND=theano \
+echo "Test keras"
+nvidia-docker run --rm ${image_name} bash -c " \
     python -c 'import keras; print(keras.__version__)'"
 echo "Test tensorflow"
 nvidia-docker run --rm ${image_name} python -c \
